8ef6a22c3b6e435080189da99e59617a5f7b1e19
Metin Simsek <metinsimsekefl@gmail.com>
2023-07-31 17:53:49 +0300

    lastcommit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const path = require('path');
 const express = require('express');
 const colors = require('colors');
@@ -12,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors())
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
